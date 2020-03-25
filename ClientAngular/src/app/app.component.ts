b9bc752c2e74e5f5f3f7d88a4dbafd9a5fbba516
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2020-03-25 15:59:22 +0000

    Inizio progetto

---
+++ ClientAngular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Unit } from './unit.model';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  obsUnit: Observable<Unit[]>; //L’observable che sta in attesa dei dati
+  data: Unit[];
+  constructor(private http: HttpClient) { } //Dependency injection
+  getUnitList(): void {
+    //Qui va sostituito l’url con quello delle vostre api
+    this.obsUnit = this.http.get<Unit[]>('https://3000-a6b28262-c431-48a8-9678-1424f89881ac.ws-eu01.gitpod.io/users');
+    //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
+    this.obsUnit.subscribe((data: Unit[]) => {this.data = data;});
+  }
+}
